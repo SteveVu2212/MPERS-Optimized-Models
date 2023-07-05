b05152667daed18cf2800a99c5c77253f302ac03
Anh Tu Vu <anhtu@DevBook.local>
2023-07-05 10:43:34 -0700

    all ARR scearios

--- Models/Analysis.R
+++ Models/Analysis.R
@@ -3,8 +3,8 @@ source("./Models/MasterModel.R")
 
 s_fund <- Sys.time()
 det_SQ_assumption_755pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.0755,
-  new_disc_rate = 0.0755,
+  curr_disc_rate = 0.06,
+  new_disc_rate = 0.06,
   cola = 0.03,
   retire_refund_ratio = 0.6,
   funding_policy = "status quo",
@@ -13,22 +13,22 @@ det_SQ_assumption_755pct <-  memoised_get_funding_data(
 )
 e_fund <- Sys.time()
 
-s_fund <- Sys.time()
-det_SQ_assumption_7pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.07,
-  new_disc_rate = 0.07,
-  cola = 0.03,
-  retire_refund_ratio = 0.6,
-  funding_policy = "status quo",
-  analysis_type = "Deterministic",
-  roa_scenario = "Assumption"
-)
-e_fund <- Sys.time()
+# s_fund <- Sys.time()
+# det_SQ_assumption_7pct <-  memoised_get_funding_data(
+#   curr_disc_rate = 0.07,
+#   new_disc_rate = 0.07,
+#   cola = 0.03,
+#   retire_refund_ratio = 0.6,
+#   funding_policy = "status quo",
+#   analysis_type = "Deterministic",
+#   roa_scenario = "Assumption"
+# )
+# e_fund <- Sys.time()
 
 
 det_SQ_recur_recession_755pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.0755,
-  new_disc_rate = 0.0755,
+  curr_disc_rate = 0.06,
+  new_disc_rate = 0.06,
   cola = 0.03,
   retire_refund_ratio = 0.6,
   funding_policy = "status quo",
@@ -36,19 +36,19 @@ det_SQ_recur_recession_755pct <-  memoised_get_funding_data(
   roa_scenario = "Recurring Recession"
 )
 
-det_SQ_recur_recession_7pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.07,
-  new_disc_rate = 0.07,
-  cola = 0.03,
-  retire_refund_ratio = 0.6,
-  funding_policy = "status quo",
-  analysis_type = "Deterministic",
-  roa_scenario = "Recurring Recession"
-)
+# det_SQ_recur_recession_7pct <-  memoised_get_funding_data(
+#   curr_disc_rate = 0.07,
+#   new_disc_rate = 0.07,
+#   cola = 0.03,
+#   retire_refund_ratio = 0.6,
+#   funding_policy = "status quo",
+#   analysis_type = "Deterministic",
+#   roa_scenario = "Recurring Recession"
+# )
 
 det_ADC_assumption_755pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.0755,
-  new_disc_rate = 0.0755,
+  curr_disc_rate = 0.06,
+  new_disc_rate = 0.06,
   cola = 0.03,
   retire_refund_ratio = 0.6,
   funding_policy = "ADC",
@@ -56,20 +56,20 @@ det_ADC_assumption_755pct <-  memoised_get_funding_data(
   roa_scenario = "Assumption"
 )
 
-det_ADC_assumption_7pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.07,
-  new_disc_rate = 0.07,
-  cola = 0.03,
-  retire_refund_ratio = 0.6,
-  funding_policy = "ADC",
-  analysis_type = "Deterministic",
-  roa_scenario = "Assumption"
-)
+# det_ADC_assumption_7pct <-  memoised_get_funding_data(
+#   curr_disc_rate = 0.07,
+#   new_disc_rate = 0.07,
+#   cola = 0.03,
+#   retire_refund_ratio = 0.6,
+#   funding_policy = "ADC",
+#   analysis_type = "Deterministic",
+#   roa_scenario = "Assumption"
+# )
 
 
 det_ADC_recur_recession_755pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.0755,
-  new_disc_rate = 0.0755,
+  curr_disc_rate = 0.06,
+  new_disc_rate = 0.06,
   cola = 0.03,
   retire_refund_ratio = 0.6,
   funding_policy = "ADC",
@@ -77,26 +77,26 @@ det_ADC_recur_recession_755pct <-  memoised_get_funding_data(
   roa_scenario = "Recurring Recession"
 )
 
-det_ADC_recur_recession_7pct <-  memoised_get_funding_data(
-  curr_disc_rate = 0.07,
-  new_disc_rate = 0.07,
-  cola = 0.03,
-  retire_refund_ratio = 0.6,
-  funding_policy = "ADC",
-  analysis_type = "Deterministic",
-  roa_scenario = "Recurring Recession"
-)
+# det_ADC_recur_recession_7pct <-  memoised_get_funding_data(
+#   curr_disc_rate = 0.07,
+#   new_disc_rate = 0.07,
+#   cola = 0.03,
+#   retire_refund_ratio = 0.6,
+#   funding_policy = "ADC",
+#   analysis_type = "Deterministic",
+#   roa_scenario = "Recurring Recession"
+# )
 
-file_path = "/Users/anhtu/Documents/Reason Org/Actuarial Modeling Projects/MS PERS/MPERS-Optimized-Models-Steve-Github/Outputs/new outputs/"
-write.xlsx(det_SQ_assumption_755pct, file=paste0(file_path, "det_SQ_assumption_755pct.xlsx"))
-write.xlsx(det_SQ_recur_recession_755pct, file=paste0(file_path, "det_SQ_recur_recession_755pct.xlsx"))
-write.xlsx(det_ADC_assumption_755pct, file=paste0(file_path, "det_ADC_assumption_755pct.xlsx"))
-write.xlsx(det_ADC_recur_recession_755pct, file=paste0(file_path, "det_ADC_recur_recession_755pct.xlsx"))
+file_path = "/Users/anhtu/Documents/Reason Org/Actuarial_Modeling_Projects/MS_PERS/MPERS-Optimized-Models-Steve-Github/Outputs/"
+write.xlsx(det_SQ_assumption_755pct, file=paste0(file_path, "det_SQ_assumption_6pct.xlsx"))
+write.xlsx(det_SQ_recur_recession_755pct, file=paste0(file_path, "det_SQ_recur_recession_6pct.xlsx"))
+write.xlsx(det_ADC_assumption_755pct, file=paste0(file_path, "det_ADC_assumption_6pct.xlsx"))
+write.xlsx(det_ADC_recur_recession_755pct, file=paste0(file_path, "det_ADC_recur_recession_6pct.xlsx"))
 
-write.xlsx(det_SQ_assumption_7pct, file=paste0(file_path, "det_SQ_assumption_7pct.xlsx"))
-write.xlsx(det_SQ_recur_recession_7pct, file=paste0(file_path, "det_SQ_recur_recession_7pct.xlsx"))
-write.xlsx(det_ADC_assumption_7pct, file=paste0(file_path, "det_ADC_assumption_7pct.xlsx"))
-write.xlsx(det_ADC_recur_recession_7pct, file=paste0(file_path, "det_ADC_recur_recession_7pct.xlsx"))
+# write.xlsx(det_SQ_assumption_7pct, file=paste0(file_path, "det_SQ_assumption_7pct.xlsx"))
+# write.xlsx(det_SQ_recur_recession_7pct, file=paste0(file_path, "det_SQ_recur_recession_7pct.xlsx"))
+# write.xlsx(det_ADC_assumption_7pct, file=paste0(file_path, "det_ADC_assumption_7pct.xlsx"))
+# write.xlsx(det_ADC_recur_recession_7pct, file=paste0(file_path, "det_ADC_recur_recession_7pct.xlsx"))
 
 
 
